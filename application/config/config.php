dc63d85f1948fcd577cafa0f2b90c8d06dfb3ba6
Achmad Nugrahantoro <38897958+nugrahantoro@users.noreply.github.com>
2019-02-07 09:05:25 +0700

    update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://45.118.112.234:10119/dashboardpp/';
+$config['base_url'] = 'http://localhost/dashboardpp/';
 
 /*
 |--------------------------------------------------------------------------
